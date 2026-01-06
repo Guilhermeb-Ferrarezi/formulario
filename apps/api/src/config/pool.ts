4976eaaf94b4cd43a566239e3729f6a49662cc0d
guilherme <guibferrarezi@gmail.com>
2026-01-06 17:24:19 -0300

    banco de dados mexido

--- apps/api/src/config/pool.ts
+++ apps/api/src/config/pool.ts
@@ -4,7 +4,9 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: false,
+  ssl: process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: false }
+    : false,
 });
--- apps/api/src/routes/aluno.routes.ts
+++ apps/api/src/routes/aluno.routes.ts
@@ -1,11 +1,33 @@
 import { Router } from "express";
-import { criarAlunoController } from "../controllers/aluno.controller";4
+import { pool } from "../config/pool";
 
 const router = Router();
 
-router.post("/alunos", criarAlunoController);
-router.get("", (req, res) => {
-  res.send("API de Alunos funcionando!");
-})
+router.post("/", async (req, res) => {
+  try {
+    const { nome, dataNascimento, whatsapp, email, cpf } = req.body;
+
+    // validação básica
+    if (!nome || !dataNascimento || !whatsapp || !email || !cpf) {
+      return res.status(400).json({ error: "Dados incompletos" });
+    }
+
+    // INSERT no banco
+    const result = await pool.query(
+      `
+      INSERT INTO alunos (nome, data_nascimento, whatsapp, email, cpf)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *
+      `,
+      [nome, dataNascimento, whatsapp, email, cpf]
+    );
+
+    return res.status(201).json(result.rows[0]);
+
+  } catch (err) {
+    console.error("❌ Erro ao inserir aluno:", err);
+    return res.status(500).json({ error: "Erro ao salvar no banco" });
+  }
+});
 
 export default router;
