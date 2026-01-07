5f4ddfdb2b1143354a9b174087b867ff4b6d383b
guilherme <guibferrarezi@gmail.com>
2026-01-07 15:25:02 -0300

    caminho frontendpath alterado

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -7,7 +7,7 @@ import authRouter from "./routes/auth.routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const frontendPath = path.resolve(process.cwd(), "../web/dist");
+const frontendPath = path.resolve(__dirname, "../../web/dist");
 
 // Verifica se o build existe
 if (!fs.existsSync(frontendPath)) {
