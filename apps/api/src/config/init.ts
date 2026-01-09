a9af9027c2a7f9d53bfc4baf32a1c213b754e307
guilherme <guibferrarezi@gmail.com>
2026-01-09 15:50:11 -0300

    banco de dados atualizado

--- apps/api/src/config/init.ts
+++ apps/api/src/config/init.ts
@@ -1,14 +1,22 @@
 import { pool } from "./pool";
 
-export async function initConfig() {
+export async function initConfig(): Promise<void> {
   await pool.query(`
     CREATE TABLE IF NOT EXISTS alunos (
       id SERIAL PRIMARY KEY,
+
       nome TEXT NOT NULL,
+
       data_nascimento DATE NOT NULL,
-      whatsapp TEXT NOT NULL,
-      email TEXT NOT NULL,
-      cpf TEXT NOT NULL
-    )
+
+      whatsapp VARCHAR(20) NOT NULL UNIQUE,
+
+      email TEXT NOT NULL UNIQUE,
+
+      cpf VARCHAR(14) NOT NULL UNIQUE,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    );
   `);
 }
